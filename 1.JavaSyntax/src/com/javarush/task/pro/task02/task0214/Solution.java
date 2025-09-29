e9dc25f11abd75139281533f13f539fcfac6ad75
Sasapupupu <sun970194@gmail.com>
2025-09-29 19:59:26 +0300

    complete task 0214

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0214/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0214/Solution.java
@@ -9,7 +9,14 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Scanner keyboard = new Scanner(System.in);
+        String FirstLine = keyboard.nextLine();
+        String SecondLine = keyboard.nextLine();
+        String ThirdLine = keyboard.nextLine();
+
+        System.out.println(ThirdLine);
+        System.out.println(SecondLine.toUpperCase());
+        System.out.println(FirstLine.toLowerCase());
 
     }
 }

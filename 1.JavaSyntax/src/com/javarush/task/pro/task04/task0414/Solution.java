1f9706e3b0a40fa63705e57e0695d91a0a0992df
Sasapupupu <sun970194@gmail.com>
2025-11-03 22:41:57 +0300

    half_complete task 0414

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0414/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task04.task0414;
+
+import java.util.Scanner;
+
+/* 
+Хорошего не бывает много
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputLine = scanner.nextLine();
+        int inputInt = scanner.nextInt();
+
+        do {
+            System.out.println(inputLine);
+            inputInt--;
+        } while (inputInt > 0 && !(inputInt > 5));
+
+    }
+}

ad484967007c43fb041f47af2b495d2de8fa3d11
Sasapupupu <sun970194@gmail.com>
2025-11-05 21:14:49 +0300

    complete task 0404

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0404/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task04.task0404;
+
+/* 
+Заполненный прямоугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int j = 5;
+        while (j > 0) {
+            int i = 10;
+            while (i > 0) {
+                System.out.print("Q");
+                i--;
+            }
+            System.out.println("");
+            j--;
+        }
+    }
+}

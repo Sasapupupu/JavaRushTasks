6ba8b1ec5e892cc4371ea2bd223e38c4ec4a0e26
Sasapupupu <sun970194@gmail.com>
2025-11-21 12:08:57 +0300

    complete task 0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+    public static void universalMethod(int[] array) {
+
+    }
+    public static void universalMethod(String[] array){
+
+    }
+    public static void universalMethod(String inputStr){
+
+    }
+    public static void universalMethod(double inputDouble){
+
+    }
+    public static void universalMethod(int inputInt){
+
+    }
+    public static void universalMethod(double[] array){
+
+    }
+    public static void universalMethod(long[] array){
+
+    }
+    public static void universalMethod(long inputLong){
+
+    }
+    public static void universalMethod(int[][] deepArray){
+
+    }
+    public static void universalMethod(String[][] deepArray){
+
+    }
+}

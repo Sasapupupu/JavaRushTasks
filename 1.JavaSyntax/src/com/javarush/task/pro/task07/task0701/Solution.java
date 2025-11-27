2eda0970a5e7fe0fd9afc1fb124c249248473b4b
Sasapupupu <sun970194@gmail.com>
2025-11-27 21:28:48 +0300

    complete task 0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    byte Byte;
+    short Short;
+    int Int;
+    long Long;
+
+    double Double;
+    float Float;
+
+    char Charter;
+    boolean Boolean;
+}

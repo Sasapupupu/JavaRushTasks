e66fe5c97116daef0dc880b1fadc83e3e810b1b6
Sasapupupu <sun970194@gmail.com>
2025-11-27 21:55:51 +0300

    complete task 0703

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    byte Byte = -128;
+    short Short = -32768;
+    int Int = Integer.MIN_VALUE;
+    long Long = -9223372036854775808L;
+}

a9d06effd8c22a3a792b564d9843ae82a7e3b5f5
Sasapupupu <sun970194@gmail.com>
2025-11-27 21:57:57 +0300

    complete task 0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    byte Byte = 127;
+    short Short = 32767;
+    int Int = Integer.MAX_VALUE;
+    long Long = 9223372036854775807L;
+}

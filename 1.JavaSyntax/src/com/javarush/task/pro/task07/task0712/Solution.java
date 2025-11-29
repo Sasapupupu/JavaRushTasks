372ee49fc7e8e1dc098cf33d69c8e25188fc5329
Sasapupupu <sun970194@gmail.com>
2025-11-29 12:33:50 +0300

    complete task 0712

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Scanner console = new Scanner(System.in);
+
+        int[] ints1 = new int[1];
+        int[] ints2 = new int[2];
+        int[] ints3 = new int[3];
+        int[] ints4 = new int[4];
+        int[] ints5 = new int[5];
+
+        String String1 = new String();
+        String String2 = new String();
+        String String3 = new String();
+        String String4 = new String();
+        String String5 = new String();
+        String String6 = new String();
+        String String7 = new String();
+        String String8 = new String();
+        String String9 = new String();
+        String String10 = new String();
+    }
+}

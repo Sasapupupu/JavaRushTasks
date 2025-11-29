2703b4f218d7d841c31960c1c61b89b6d4f07648
Sasapupupu <sun970194@gmail.com>
2025-11-29 19:41:41 +0300

    complete task 0801

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0801/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task08.task0801;
+
+import static java.lang.Math.toRadians;
+
+/* 
+Утильный класс: часть 1
+*/
+
+public class Solution {
+
+    public static double sin(double a) {
+        double output = Math.sin(Math.toRadians(a));
+        return output;
+    }
+
+    public static double cos(double a) {
+        double output = Math.cos(Math.toRadians(a));
+        return output;
+    }
+
+    public static double tan(double a) {
+        double output = Math.tan(Math.toRadians(a));
+        return output;
+    }
+}

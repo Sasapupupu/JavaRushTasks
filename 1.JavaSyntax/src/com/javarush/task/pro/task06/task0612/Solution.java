5a6629629edd2983b0c98696906a15eab8b95a61
Sasapupupu <sun970194@gmail.com>
2025-12-31 15:32:40 +0300

    complete task 0612

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0612/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0612/Solution.java
@@ -11,11 +11,11 @@ public class Solution {
     }
 
     public static void printSqrt(int[] array) {
-        String elementStrSqrt = "Корень квадратный для числа ";
+        String elementSqrt = "Корень квадратный для числа ";
         for (int i = 0; i < array.length; i++) {
             int element = array[i];
-            double elementSqrt = Math.sqrt(element);
-            System.out.println(elementSqrt + element + " равен " + elementSqrt);
+            double Sqrt = Math.sqrt(element);
+            System.out.println(elementSqrt + element + " равен " + Sqrt);
         }
     }
 }
